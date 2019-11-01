7c4459c86522d99bebc7cbd05d2655aef1954769
Andrei <a981203@mail.ru>
2019-11-01 01:39:34 -0700

    com

--- app/src/main/java/com/example/imho/MapActivity.java
+++ app/src/main/java/com/example/imho/MapActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MapActivity extends AppCompatActivity {
-
+//sa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
